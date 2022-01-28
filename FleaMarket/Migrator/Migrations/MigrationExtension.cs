5674c1a3d87cfd41ee4e60bc733f0343a1c2ad4b
Valer4ik <66671751+Valer4ikOnlyPC@users.noreply.github.com>
2022-01-28 18:43:01 +0300

    Migration

---
+++ FleaMarket/Migrator/Migrations/MigrationExtension.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator.Runner;
+using FluentMigrator;
+using FluentMigrator.Postgres;
+using FluentMigrator.Exceptions;
+
+namespace Migrator.Migrations
+{
+    public static class MigrationExtension
+    {
+        public static IApplicationBuilder Migrate(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+            runner.MigrateUp(20220128164600);
+            return app;
+        }
+    }
+}
---
+++ FleaMarket/Migrator/Migrations/Migration_20200601100000.cs
@@ -0,0 +1,87 @@
+﻿using FluentMigrator.Runner;
+using FluentMigrator.Postgres;
+using FluentMigrator;
+
+namespace Migrator.Migrations
+{
+    [Migration(20220128164600)]
+    public class Migration_20200601100000 : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            Create.Table("Citys")
+                .WithColumn("CityId").AsInt32().NotNullable().Identity().PrimaryKey()
+                .WithColumn("\"Name\"").AsString().NotNullable();
+
+            Create.Table("UserPasswords")
+                .WithColumn("UserPasswordId").AsGuid().Identity().PrimaryKey()
+                .WithColumn("\"Password\"").AsString().NotNullable();
+
+            Create.Table("Users")
+                .WithColumn("UserId").AsGuid().Identity().PrimaryKey()
+                .WithColumn("Surname").AsString().NotNullable()
+                .WithColumn("\"Name\"").AsString().NotNullable()
+                .WithColumn("PhoneNumber").AsString().NotNullable()
+                .WithColumn("VkAddress").AsString().Nullable()
+                .WithColumn("Rating").AsInt32()
+                .WithColumn("CityId").AsInt32().NotNullable().ForeignKey("Citys", "CityId")
+                .WithColumn("IsDeleted").AsBoolean().NotNullable()
+                .WithColumn("PasswordId").AsGuid().NotNullable().Unique().ForeignKey("UserPasswords", "UserPasswordId");
+
+            Create.Index("IX_Passwords_Users")
+                .OnTable("Users")
+                .OnColumn("PasswordId")
+                .Ascending()
+                .WithOptions().NonClustered();
+
+            Create.Index("IX_Citys_Users")
+                .OnTable("Citys")
+                .OnColumn("CityId")
+                .Ascending()
+                .WithOptions().NonClustered();
+
+            Create.Table("Categorys")
+                .WithColumn("CategoryId ").AsInt32().NotNullable().Identity().PrimaryKey()
+                .WithColumn("\"Name\"").AsString().NotNullable()
+                .WithColumn("CategoryParent").AsInt32().NotNullable();
+
+            Create.Table("Products")
+                .WithColumn("ProductId").AsGuid().Identity().PrimaryKey()
+                .WithColumn("\"Name\"").AsString().NotNullable()
+                .WithColumn("FirstPhoto").AsString().NotNullable()
+                .WithColumn("Description").AsString().NotNullable()
+                .WithColumn("CityId").AsInt32().NotNullable().ForeignKey("Citys", "CityId")
+                .WithColumn("IsActive").AsInt32().NotNullable()
+                .WithColumn("CategoryId").AsInt32().NotNullable().ForeignKey("Categorys", "CategoryId")
+                .WithColumn("UserId").AsGuid().NotNullable().ForeignKey("Users", "UserId");
+
+            Create.Index("IX_Citys_Products")
+                .OnTable("Citys")
+                .OnColumn("CityId")
+                .Ascending()
+                .WithOptions().NonClustered();
+
+            Create.Index("IX_Categorys_Products")
+                .OnTable("Categorys")
+                .OnColumn("CategoryId")
+                .Ascending()
+                .WithOptions().NonClustered();
+
+            Create.Table("ProductPhotos")
+                .WithColumn("PhotoId").AsGuid().Identity().PrimaryKey()
+                .WithColumn("\"Link\"").AsString().NotNullable()
+                .WithColumn("ProductId").AsGuid().NotNullable().ForeignKey("Products", "ProductId");
+
+            Create.Index("IX_Products_Photos")
+                .OnTable("Products")
+                .OnColumn("ProductId")
+                .Ascending()
+                .WithOptions().NonClustered();
+        }
+    }
+}
